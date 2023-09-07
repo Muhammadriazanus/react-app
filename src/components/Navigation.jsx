768d9be85cd58b0e679001a86413a9970618f60b
muhammadriazanus <riazanus568@gmail.com>
2023-09-07 05:12:39 -0700

    first commit

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,19 @@
+const Navigation = ()=>{
+    return (
+        <nav className="container">
+      <div className="logo">
+        <img src="/images/brand_logo.png" alt="logo" />
+
+      </div>
+      <ul>
+        <li herf="#">Menu</li>
+        <li herf="#">location</li>
+        <li herf="#">About</li>
+        <li herf="#">contact</li>
+      </ul>
+      <button>login</button>
+    </nav>
+    )
+    
+}
+export default Navigation;
---
+++ src/components/hero.jsx
@@ -0,0 +1,30 @@
+const HeroSection = ()=>{
+    return <main className="hero container">
+        <div className="hero-content">
+            <h1>YOUR FEET DESERVE THE REST</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt quasi dignissimos error quis, harum rem commodi enim quisquam explicabo, assumenda neque quas officiis nam id!</p>
+
+
+        <div className="shopping">
+            <p>also Available On </p>
+        <button className="hero-btn">shop Now</button>
+        <button className="secondary-btn">category</button>
+        </div>
+
+        <div className="brand-icons">
+            <img src="/images/amazon.png" alt="amazon" />
+            <img src="/images/flipkart.png" alt="flipkart-logo" />
+        </div>
+        </div>
+
+
+        <div className="hero-images">
+            <img src="/images/hero-image.png" alt="hero-images" />
+        </div>
+
+    </main>
+    
+}
+export default HeroSection
+
+
